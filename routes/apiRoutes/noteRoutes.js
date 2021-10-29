0c64b9711e842afe598ccdee927f45ebec7e3d24
charlie1099 <cdcharlesworth@gmail.com>
2021-10-28 22:48:03 -0600

    added delet route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,47 +1,68 @@
 const router = require("express").Router()
 const { filterByQuery, findById, createNewNote, validateNote } = require("../../lib/notes")
-const { notes } = require("../../data/notes.json")
+const { notes } = require("../../db/db.json")
 const fs = require("fs")
 const path = require("path")
 
 router.get("/notes", (req, res) => {
-    let results = notes;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
 });
 
 router.get('/notes/:id', (req, res) => {
-     let results = notes;
-    const result = findByTitle(req.params.id, notes);
-    if (result) {
-      res.json(results);
-    } else {
-      res.send(404);
-    }
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
   });
 
 
-router.post('/notes', (req, res) => {
-   
-    req.body.id = notes.length.toString();
-  
-    
-    if (!validateNote(req.body)) {
-      res.status(400).send('This note is not formatted right.');
-    } else {
-      const note = createNewNote(req.body, notes);
-      res.json(note);
+  router.post("/notes", (req, res) => {
+
+    const newNote = req.body;
+
+    if (notes.length === 0){newNote.id = 1} 
+      else {
+      newNote.id = (notes[notes.length-1].id + 1);
     }
-  });
+    notes.push(newNote);
+
+    const jsonNotes = JSON.stringify(notes)
+
+    fs.writeFile("./db/db.json", jsonNotes, function(error) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("New note created");
+    })
+    res.json(notes)
+  })
   
-// router.delete("/notes/:id", (req, res) => {
-//     const result = findById(req.params.id, notes);
-//     if(result) {
-//       res.json(result);  
-//     } else {
-//         res.send(404)
-//     }
-// });
+router.delete("/db/:id", (req, res) => {
+  const noteId = req.params.id;
+
+  notes.forEach((n, index) => {
+    if(noteId == n.id){notes.splice(index,1)
+
+      const notesCopy = notes.slice();
+      
+      const jsonNotes = JSON.stringify(notesCopy)
+
+      fs.writeFile("./db/db.json", jsonNotes, function(error) {
+        if (error) {
+          return console.log(error);
+        }
+        console.log("Note Deleted");
+      })
+
+    }
+  })
+  res.json(notes);
+})
+
 module.exports = router
